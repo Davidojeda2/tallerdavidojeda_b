0651c85eab84527d58ebb476850ab9203f13dc9b
Davidojeda2 <136102238+Davidojeda2@users.noreply.github.com>
2023-06-16 17:39:31 +0000

    taller 2

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+import matematicas
+from geometria import distancia
+
+print("sqrt:",math.sqrt(25))
+print("Sumar:",matematicas.sumar(2, 3))
+print("Restar:",matematicas.restar(2, 3))
+print("multiplicar:",matematicas.multiplicar(2, 3))
+print("dividir:,",matematicas.dividir(2, 3))
+print("distancia:", distancia(1, 1, 3, 4))
